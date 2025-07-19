a5a5a9d0bea563df5074acf28d00832e7bdf3dd9
SadeepaNHerath <sadeepahearth@gmail.com>
2025-07-19 09:48:41 +0530

    Update authentication providers and improve import paths

--- auth.ts
+++ auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
+import Facebook from "next-auth/providers/facebook"
+
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Google,GitHub,Facebook],
 })
 
